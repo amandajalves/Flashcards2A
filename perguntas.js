da852e67c94884b680f5484767c9b5892dd8f1ae
amandajalves <127255909+amandajalves@users.noreply.github.com>
2024-10-21 11:44:27 -0300

    1

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criarCartao{
+    'Programação'
+    'O que é JavaScript?'
+    'O JavaScript é uma linguagem de programação'
+}
+
+criarCartao{
+    'Programação'
+    'O que é JavaScript?'
+    'O JavaScript é uma linguagem de programação'
+}
+
+criarCartao{
+    'Programação'
+    'O que é JavaScript?'
+    'O JavaScript é uma linguagem de programação'
+}
+
+
+
