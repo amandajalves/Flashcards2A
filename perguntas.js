472f5152fc947e9f668e58b2b3dedd5249db2b67
amandajalves <127255909+amandajalves@users.noreply.github.com>
2024-10-28 10:35:07 -0300

    perguntas

--- perguntas.js
+++ perguntas.js
@@ -1,20 +1,54 @@
-criarCartao{
-    'Programação'
-    'O que é JavaScript?'
-    'O JavaScript é uma linguagem de programação'
-}
+criarCartao(
+    'Programação',
+    'O que é uma variável?',
+    'Uma variável é um espaço na memória que armazena dados temporários e pode ser alterado durante a execução do programa. Ela é identificada por um nome e possui um valor, como um número ou uma string.'
+)
 
-criarCartao{
-    'Programação'
-    'O que é JavaScript?'
-    'O JavaScript é uma linguagem de programação'
-}
+criarCartao(
+    'Programação',
+    'Qual é a diferença entre um “loop for” e um “loop while”?',
+    'O “loop for” é usado quando sabemos o número de vezes que o loop deve ser repetido. O “loop while” repete enquanto uma condição específica for verdadeira, sendo útil para quando não sabemos quantas vezes o loop será executado.'
+) 
 
-criarCartao{
-    'Programação'
-    'O que é JavaScript?'
-    'O JavaScript é uma linguagem de programação'
-}
+criarCartao(
+    'Programação',
+    'Para que serve uma função?',
+    'Uma função é um bloco de código que realiza uma tarefa específica e pode ser chamada várias vezes dentro do programa, facilitando a organização e a reutilização do código.'
+)
 
+criarCartao(
+    'Matemática',
+    'O que é uma fração equivalente?',
+    'Frações equivalentes representam a mesma quantidade ou valor, mesmo que tenham numeradores e denominadores diferentes.'
+)
 
+criarCartao(
+    'Matemática',
+    'Como calculamos a média aritmética de um conjunto de números?',
+    'A média aritmética é calculada somando todos os valores e dividindo o resultado pela quantidade de valores.'
+) 
+
+criarCartao(
+    'Matemática',
+    'Qual é a fórmula para calcular a área de um triângulo?',
+    'A área de um triângulo é dada pela fórmula Área=(basexaltura)/2'
+)
+
+criarCartao(
+    'Educação Financeira',
+    'O que é um orçamento pessoal?',
+    'Um orçamento pessoal é um planejamento financeiro que ajuda a controlar os gastos e economias, organizando receitas e despesas mensais para alcançar objetivos financeiros.'
+)
+
+criarCartao(
+    'Educação Financeira',
+    'Qual é a diferença entre um gasto fixo e um gasto variável?',
+    'Um gasto fixo é uma despesa que ocorre regularmente e com o mesmo valor, como o aluguel. Já um gasto variável pode mudar de valor e frequência, como lazer ou alimentação fora de casa.'
+) 
+
+criarCartao(
+    'Educação Financeira',
+    'O que significa a sigla CDB em investimentos?',
+    'CDB significa Certificado de Depósito Bancário. É um tipo de investimento em que o investidor empresta dinheiro ao banco, recebendo juros sobre esse valor após um período acordado.'
+)
 
